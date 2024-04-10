2c58eab28afe989ba82db6eac9d7a549478adda3
Soham-27 <phadkesoham2004@gmail.com>
2024-04-10 16:56:26 +0530

    commit for test:5

--- src/index.js
+++ src/index.js
@@ -23,18 +23,41 @@ const readCSV = require('./csvReader');
 //     });
 // }
 
+// async function executeSELECTQuery(query) {
+//     const { fields, table } = parseQuery(query);
+//     const data = await readCSV(`${table}.csv`);
+    
+//     // Filter the fields based on the query
+//     return data.map(row => {
+//         const filteredRow = {};
+//         fields.forEach(field => {
+//             filteredRow[field] = row[field];
+//         });
+//         return filteredRow;
+//     });
+// }
 async function executeSELECTQuery(query) {
-    const { fields, table } = parseQuery(query);
+    const { fields, table, whereClause } = parseQuery(query);
     const data = await readCSV(`${table}.csv`);
     
-    // Filter the fields based on the query
-    return data.map(row => {
-        const filteredRow = {};
+    // Filtering based on WHERE clause
+    const filteredData = whereClause
+        ? data.filter(row => {
+            const [field, value] = whereClause.split('=').map(s => s.trim());
+            return row[field] === value;
+        })
+        : data;
+
+    // Selecting the specified fields
+    return filteredData.map(row => {
+        const selectedRow = {};
         fields.forEach(field => {
-            filteredRow[field] = row[field];
+            selectedRow[field] = row[field];
         });
-        return filteredRow;
+        return selectedRow;
     });
 }
+
+module.exports = executeSELECTQuery;
 
 module.exports = executeSELECTQuery;
--- src/queryParser.js
+++ src/queryParser.js
@@ -1,17 +1,34 @@
 // src/queryParser.js
+// function parseQuery(query) {
+//     const selectRegex = /SELECT (.+) FROM (.+)/i;
+//     const match = query.match(selectRegex);
+
+//     if (match) {
+//         const [, fields, table] = match;
+//         return {
+//             fields: fields.split(',').map(field => field.trim()),
+//             table: table.trim()
+//         };
+//     } else {
+//         throw new Error('Invalid query format');
+//     }
+// }
 function parseQuery(query) {
-    const selectRegex = /SELECT (.+) FROM (.+)/i;
+    const selectRegex = /SELECT (.+?) FROM (.+?)(?: WHERE (.*))?$/i;
     const match = query.match(selectRegex);
 
     if (match) {
-        const [, fields, table] = match;
+        const [, fields, table, whereClause] = match;
         return {
             fields: fields.split(',').map(field => field.trim()),
-            table: table.trim()
+            table: table.trim(),
+            whereClause: whereClause ? whereClause.trim() : null
         };
     } else {
         throw new Error('Invalid query format');
     }
 }
+
+module.exports = parseQuery;
 
 module.exports = parseQuery;
